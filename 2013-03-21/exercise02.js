5a09ef9b4d0c14c09ad3098ed32742f10ca9d404
francesco-dipi <Francesco.Dipi@gmail.com>
2013-03-25 18:03:20 +0100

    esercizi

--- 2013-03-21/exercise02.js
+++ 2013-03-21/exercise02.js
@@ -7,14 +7,13 @@
 function Edge(p1, p2) {
 	this.p1 = p1;
 	this.p2 = p2;
-	
-	/*
-	 *  exercise02b
-	 * 
-	 * Write a method length for Edge
-	 * that compute the length of the edge.
-	 */
-	this.length = function() {
-		return Math.sqrt(Math.pow(this.p1.x - this.p2.x, 2) + Math.pow(this.p1.y - this.p2.y, 2));
-	}
+}
+/*
+ *  exercise02b
+ * 
+ * Write a method length for Edge
+ * that compute the length of the edge.
+ */
+Edge.prototype.length = function() {
+	return Math.sqrt(Math.pow(this.p1.x - this.p2.x, 2) + Math.pow(this.p1.y - this.p2.y, 2));
 }
--- 2013-03-21/exercise03.js
+++ 2013-03-21/exercise03.js
@@ -8,24 +8,24 @@ function Triangle(e1, e2, e3) {
 	this.edge1 = e1;
 	this.edge2 = e2;
 	this.edge3 = e3;
-	
-	/*
-	 *  exercise03b
-	 * 
-	 * Write a method perimeter for Triangle
-	 * that compute the perimeter of the triangle.
-	 */
-	this.perimeter = function() {
-		return this.edge1.length() + this.edge2.length() + this.edge3.length();
-	}
-	
-	/*  exercise03b
-	 * 
-	 * Write a method area for Triangle
-	 * that compute the area of the triangle
-	 */
-	this.area = function() {
-		var p = this.perimeter()/2;
-		return Math.sqrt(p * (p-this.edge1.length()) * (p-this.edge2.length()) * (p-this.edge3.length()));
-	}
+}
+
+/*
+ *  exercise03b
+ * 
+ * Write a method perimeter for Triangle
+ * that compute the perimeter of the triangle.
+ */
+Triangle.prototype.perimeter = function() {
+	return this.edge1.length() + this.edge2.length() + this.edge3.length();
+}
+
+/*  exercise03b
+ * 
+ * Write a method area for Triangle
+ * that compute the area of the triangle
+ */
+Triangle.prototype.area = function() {
+	var p = this.perimeter()/2;
+	return Math.sqrt(p * (p-this.edge1.length()) * (p-this.edge2.length()) * (p-this.edge3.length()));
 }
